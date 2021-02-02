041860e026f1a70755e23427d54b3ac7d395f2c3
Arylmera <guillaume@lemer.be>
2021-02-02 12:14:03 +0100

    service
    Co-Authored-By: BrianLuk10 <56024639+BrianLuk10@users.noreply.github.com>

--- Front-End/bedrommSite/src/app/services/appointmentService/appointment.service.ts
+++ Front-End/bedrommSite/src/app/services/appointmentService/appointment.service.ts
@@ -5,9 +5,18 @@ import { Injectable } from '@angular/core';
 })
 export class AppointmentService {
 
+  date_list = [
+    {date: "2015-10-12 12:15"},
+    {date: "2015-10-12 13:55"},
+    {date: "2015-10-12 16:35"},
+    {date: "2015-10-12 17:25"},
+    {date: "2015-10-13 14:55"},
+    {date: "2015-10-13 08:25"},
+    ];
+
   constructor() { }
 
   getDateList(Date){
-    return this.dateList;
+    return this.date_list;
   }
 }
