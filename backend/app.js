9b351dbf79206436472723c74255d60e2d79095e
PATRICKTCHOUPE2TL1 <patricktchoupe85@gmail.com>
2021-02-02 19:07:17 +0100

    ajout de docker

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const dotenv = require("dotenv");
 const path = require('path');
 
-const dbConnection = require("../backend/config/database");
+const dbConnection = require("./config/database");
 
 const app = express();
 dotenv.config();
@@ -30,14 +30,22 @@ app.get('/', (req, res) => {
         .join(__dirname + '/public', 'dist', 'index.html'))
 });
 
+
+
 // Bring in the user routers
 const clients = require('./routes/clients');
 
 app.use('/api/v1', clients);
 
+app.get('*', (req, res) => {
+    return res.sendFile(path
+      .join(__dirname + '/public', 'dist', 'index.html'))
+  })
 
 app.listen(process.env.PORT, () => {
     console.log(`Server started on port ${process.env.PORT}`);
 });
+
+
 
 
