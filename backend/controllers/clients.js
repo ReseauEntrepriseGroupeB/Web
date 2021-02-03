66c28327e3acdbdb59d97cfac9db26999c96d8fb
danny00747 <morganz55057@gmail.com>
2021-02-03 18:34:18 +0100

    fixed cron bug

--- backend/controllers/clients.js
+++ backend/controllers/clients.js
@@ -1,9 +1,5 @@
 const Client = require("../models/clients");
-const Op = require("sequelize");
 const cron = require('node-cron');
-const Sequelize = require("sequelize");
-var moment = require('moment');
-
 
 const clients = async (req, res) => {
     const users = await Client.findAll(
@@ -33,15 +29,6 @@ const getDate = async (req, res) => {
 
     const reqBodyDate = req.params.date;
 
-    const t = await Client.findAll(
-        {
-            where:
-                {
-                    dateRDV:
-                        {$between: [reqBodyDate + ' 00:00:00.000000 +00:00', +reqBodyDate + ' 23:59:59.000000 +00:00']}
-                }
-        });
-
     const result = await Client.findAll(
         {
             attributes: ['nom', 'dateRDV', 'email']
@@ -49,8 +36,8 @@ const getDate = async (req, res) => {
 
     const filterredDates = result
         .filter(x => (new Date(x.dateRDV).getFullYear()).toString() === reqBodyDate.split('-')[0]
-        && (new Date(x.dateRDV).getMonth() + 1).toString() === reqBodyDate.split('-')[1]
-        && (new Date(x.dateRDV).getDate()).toString() === reqBodyDate.split('-')[2])
+            && (new Date(x.dateRDV).getMonth() + 1).toString() === reqBodyDate.split('-')[1]
+            && (new Date(x.dateRDV).getDate()).toString() === reqBodyDate.split('-')[2])
         .map(x => new Date(x.dateRDV).getHours() - 1);
 
     let set = new Set();
@@ -60,21 +47,25 @@ const getDate = async (req, res) => {
         .json({rdv: Array.from(set)});
 };
 
-function dayDiff(d1, d2) { return new Number((d2.getTime() - d1.getTime()) / 31536000000).toFixed(0); }
+function dayDiff(d1, d2) {
+    return Number((d2.getTime() - d1.getTime()) / 31536000000).toFixed(0);
+}
 
-cron.schedule('* * 24 * * *', async function() {
-    const time = await Client.findAll()
-    time.forEach(x => {
+cron.schedule('59 23 * * *', async () => {
+
+    const clients = await Client.findAll();
+
+    clients.forEach(x => {
         const day1 = new Date(x.dataValues.createdAt);
         const day2 = new Date();
         x.diff = dayDiff(day1, day2);
     });
-    const timefiltered = time.filter(x => x.diff > 3).map(x => x.id)
 
-    Client.destroy({ where: { id: [...timefiltered] }})
+    const timefiltered = clients.filter(x => x.diff > 3).map(x => x.id)
 
-    });
+    Client.destroy({where: {id: [...timefiltered]}});
 
+});
 
 
 module.exports = {
